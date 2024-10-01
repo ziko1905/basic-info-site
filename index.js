14c8ae3b377cc7fc1500e5fc74a0d6c9be08417e
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-01 22:41:18 +0200

    Route to proper pathname or error

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const url = require('node:url')
 http.createServer((req, res) => {
     //Getting the pathname of url
     const q = url.parse(req.url, true)
-    const pathname = '.' + q.pathname
+    const pathname = "." + q.pathname
     if (pathname == "./") {
         fs.readFile("./index.html", (err, data) => {
             if (err) throw err
-            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.writeHead(200, { "Content-Type": "text/html"})
+            res.write(data)
+            return res.end()
+        })
+    }
+    else {
+        fs.readFile(`${pathname}.html`, (err, data) => {
+            if (err) {
+                return fs.readFile("404.html", (err, data) => {
+                    if (err) throw err
+                    res.writeHead(404, { "Content-Type": 'text/html' })
+                    res.write(data)
+                    res.end()
+                })
+            }
+            res.writeHead(200, { "Content-Type": 'text/html' })
             res.write(data)
             return res.end()
         })
