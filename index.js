d7a5c72b005f9ff56f391b0c69037b754865b7ac
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-01 22:23:14 +0200

    Create server and get pathname

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const http = require('node:http')
 const fs = require('node:fs')
 const url = require('node:url')
 
+http.createServer((req, res) => {
+    //Getting the pathname of url
+    const q = url.parse(req.url, true)
+    const pathname = q.pathname
+}).listen()
