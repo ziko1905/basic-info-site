02e12cf32bc568b908e8d933be0e3e65d61b86e9
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-01 22:33:27 +0200

    Route server to homepage

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const url = require('node:url')
 http.createServer((req, res) => {
     //Getting the pathname of url
     const q = url.parse(req.url, true)
-    const pathname = q.pathname
-}).listen()
+    const pathname = '.' + q.pathname
+    if (pathname == "./") {
+        fs.readFile("./index.html", (err, data) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    }
+}).listen(8080)
